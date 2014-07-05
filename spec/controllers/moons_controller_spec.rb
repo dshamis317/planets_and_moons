7d877f533153043b9091e280597dbb4caa0dd9e6
dshamis317 <dshamis317@gmail.com>
2014-07-05 17:07:56 -0400

    wrote moon GET edit, POST update and DELETE destroy tests

--- spec/controllers/moons_controller_spec.rb
+++ spec/controllers/moons_controller_spec.rb
@@ -62,6 +62,65 @@ describe MoonsController do
 
     end # GET moons show
 
+    describe 'GET edit' do
+
+      before :each do
+        get :edit, :id => @moon.id, :planet_id => @mars.id
+      end
+
+      it 'responds successfully' do
+        actual = response.code
+        expected = '200'
+        expect(actual).to eq(expected)
+      end
+
+      it 'assigns @moon' do
+        actual = assigns(:moon)
+        expected = @moon
+        expect(actual).to eq(expected)
+      end
+
+      it 'renders the edit template' do
+        expect(response).to render_template('edit')
+      end
+
+    end # GET moons edit
+
+    describe 'POST update' do
+
+      before :each do
+        post :update, :id => @moon.id, :moon => {name: 'M00N'}, :planet_id => @mars.id
+      end
+
+      it 'responds with a redirect' do
+        actual = response.code
+        expected = '302'
+        expect(actual).to eq(expected)
+      end
+
+      it 'updates moon record' do
+        @moon.reload
+        actual = @moon.name
+        expected = 'M00N'
+        expect(actual).to eq(expected)
+      end
+
+      it 'redirects to show' do
+        response.should redirect_to planet_moon_path(@mars, @moon)
+      end
+
+    end # POST moons update
+
+    describe 'DELETE destroy' do
+
+      it 'destroys moon record' do
+        expect {delete :destroy, {:id => @moon.id}, {:planet_id => @mars.id}}
+        .to change(Moon, :count)
+        .by(-1)
+      end
+
+    end
+
   end
 
 end
