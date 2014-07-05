4103ed31f6d14f995752eb38fe84374886ba1682
dshamis317 <dshamis317@gmail.com>
2014-07-05 15:12:57 -0400

    wrote get moons show tests

--- spec/controllers/moons_controller_spec.rb
+++ spec/controllers/moons_controller_spec.rb
@@ -14,7 +14,8 @@ describe MoonsController do
       @moon = Moon.new
       @moon.name = 'Moon'
       @moon.image_url = 'http://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg'
-      @moon.planet_id = 1
+      @moon.planet_id = @mars.id
+      @moon.save
     end
 
     describe 'GET index' do
@@ -37,7 +38,29 @@ describe MoonsController do
 
     end # GET moons index
 
+    describe 'GET show' do
 
+      before :each do
+        get :show, :id => @moon.id, :planet_id => @mars.id
+      end
+
+      it 'responds successfully' do
+        actual = response.code
+        expected = '200'
+        expect(actual).to eq(expected)
+      end
+
+      it 'assigns @moon' do
+        actual = assigns(:moon)
+        expected = @moon
+        expect(actual).to eq(expected)
+      end
+
+      it 'renders the show template' do
+        expect(response).to render_template('show')
+      end
+
+    end # GET moons show
 
   end
 
