c54043c2fbad255c5814e673e2b447e35ddf40aa
dshamis317 <dshamis317@gmail.com>
2014-07-05 18:02:55 -0400

    wrote test for moon model

--- app/models/moon.rb
+++ app/models/moon.rb
@@ -1,3 +1,8 @@
 class Moon < ActiveRecord::Base
   belongs_to :planet
+
+  def introduction
+    "I am a moon called #{self.name}, you could see me here: #{self.image_url}"
+  end
+
 end
--- spec/models/moon_spec.rb
+++ spec/models/moon_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Moon do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+  before :each do
+    @mars = Planet.new
+    @mars.name = 'Mars'
+    @mars.life = false
+    @mars.image_url = 'http://space-facts.com/wp-content/uploads/mars.jpg'
+    @mars.save
+  end
+
+    describe "#introduction" do
+
+      it "introduces itself" do
+        moon = Moon.new({name: "Moon", image_url: "http://placekitten.com/200/200"})
+        actual = moon.introduction
+        expected = "I am a moon called Moon, you could see me here: http://placekitten.com/200/200"
+        expect(actual).to eq(expected)
+      end
+
+    end
+  end
