45715b3d975f81367bc5d3fb2a556d2999c5f070
dshamis317 <dshamis317@gmail.com>
2014-07-05 14:23:29 -0400

    touched planets/index.html.erb view and added a create action to the planets controller

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -4,4 +4,15 @@ class PlanetsController < ApplicationController
     @planets = Planet.all
   end
 
+  def create
+    new_planet = Planet.create(planet_params)
+    redirect_to planet_path(new_planet)
+  end
+
+  private
+
+  def planet_params
+    params.require(:planet).permit(:name, :life, :image_url)
+  end
+
 end
