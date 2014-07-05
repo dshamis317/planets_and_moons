ba391071f9476b09c22cc4421bec2de23ce96aa0
dshamis317 <dshamis317@gmail.com>
2014-07-05 14:38:19 -0400

    created introduction method for planets with test in planet_spec

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,3 +1,7 @@
 class Planet < ActiveRecord::Base
   has_many :moons
+
+  def introduction
+    "I am planet #{self.name}, you could see me here: #{self.image_url}"
+  end
 end
--- spec/models/planet_spec.rb
+++ spec/models/planet_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Planet do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#introduction" do
+
+    it "introduces itself" do
+      earth = Planet.new({name: "Earth", life: true, image_url: "http://upload.wikimedia.org/wikipedia/commons/6/6f/Earth_Eastern_Hemisphere.jpg"})
+      actual = earth.introduction
+      expected = "I am planet Earth, you could see me here: http://upload.wikimedia.org/wikipedia/commons/6/6f/Earth_Eastern_Hemisphere.jpg"
+      expect(actual).to eq(expected)
+    end
+
+  end
+
 end
