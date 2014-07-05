c2d15a88e81a02c71465e80a9303cbf7427164a6
dshamis317 <dshamis317@gmail.com>
2014-07-05 14:56:05 -0400

    added nested resources for moons and began writing moons controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :planets
+
+  resources :planets do
+    resources :moons
+  end
+
 end
--- spec/controllers/moons_controller_spec.rb
+++ spec/controllers/moons_controller_spec.rb
@@ -2,4 +2,43 @@ require 'spec_helper'
 
 describe MoonsController do
 
+  describe 'given a moon' do
+
+    before :each do
+      @mars = Planet.new
+      @mars.name = 'Mars'
+      @mars.life = false
+      @mars.image_url = 'http://space-facts.com/wp-content/uploads/mars.jpg'
+      @mars.save
+
+      @moon = Moon.new
+      @moon.name = 'Moon'
+      @moon.image_url = 'http://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg'
+      @moon.planet_id = 1
+    end
+
+    describe 'GET index' do
+
+      before :each do
+        get :index, :planet_id => @mars.id
+      end
+
+      it 'responds successfully' do
+        actual = response.code
+        expected = '200'
+        expect(actual).to eq(expected)
+      end
+
+      it 'assigns @moons' do
+        actual = assigns(:moons)
+        expected = [@moon]
+        expect(actual).to eq(expected)
+      end
+
+    end # GET moons index
+
+
+
+  end
+
 end
